3f81d167eed7a797b3bbf2a9679cb9dcbff55812
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-03-23 20:03:28 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300033/Program.cs
+++ tpmodul6_103022300033/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Muhammad_Ilham_Ridzuan");
+        video.IncreasePlayCount(10); 
+        video.PrintVideoDetails();
+    }
+}
